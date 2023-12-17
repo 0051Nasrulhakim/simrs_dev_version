cba43155ddad45bfc288708bc5c1c00b1d7af27a
0051Nasrulhakim <ti192400051@gmail.com>
2023-12-17 17:02:36 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: {
-        // Base path untuk GitHub Pages
-        basePath: '/simrs_dev_version', // Sesuaikan dengan base path dari URL GitHub Pages Anda
-      },
+    output: 'export',
 }
 
 // module.exports = nextConfig
