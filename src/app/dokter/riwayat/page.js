80ebf48bc197489462624d820c2376b172bdeee2
0051Nasrulhakim <ti192400051@gmail.com>
2023-12-19 19:31:20 +0700

    Update page.js

--- src/app/dokter/riwayat/page.js
+++ src/app/dokter/riwayat/page.js
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className='border mt-[-0.6%] border-t-1 border-sky-900'>
       <div className='ml-4 mr-4 mt-3 mb-2'>
-        <div>page riwayat tes dev</div>
+        <div>page riwayat tes dev ..</div>
       </div>
     </div>
   )
