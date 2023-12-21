61b6c6b8e56994190bc2bcae8470da32d2112ae9
0051Nasrulhakim <ti192400051@gmail.com>
2023-12-21 19:59:59 +0700

    tes table

--- src/app/dokter/tabelDashDokter.js
+++ src/app/dokter/tabelDashDokter.js
@@ -3,6 +3,16 @@ import React, { useState, useEffect } from 'react'
 
 export default function TabelDashDokter() {
   const [customers, setCustomers] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const tesclick = (e) => {
+    setSelectedIndex(e);
+  }
+  const handleItemDoubleClick = (index) => {
+    // Tambahkan logika atau tindakan yang ingin Anda lakukan saat elemen di-double click
+    alert(index)
+    console.log(`Melihat detail untuk item dengan indeks: ${index}`);
+  };
 
   useEffect(() => {
     const newCustomer = [
@@ -102,28 +112,33 @@ export default function TabelDashDokter() {
   }, []);
 
   return (
-    <div class="overflow-x-auto">
-      <table class="w-full whitespace-nowrap">
+    <div className="overflow-x-auto">
+      <table className="w-full whitespace-nowrap">
         <thead>
-          <tr class="text-left text-gray-600 bg-gray-100">
-            <th class="px-4 py-2 w-1/12 border-b border-gray-200">ID</th>
-            <th class="px-4 py-2 w-1/3 border-b border-gray-200">Name</th>
-            <th class="px-4 py-2 w-1/6 border-b border-gray-200">Negara</th>
-            <th class="px-4 py-2 w-2/12 border-b border-gray-200">Email</th>
+          <tr className="text-left text-gray-600 bg-gray-100">
+            <th className="px-4 py-2 w-1/12 border-b border-gray-200">ID</th>
+            <th className="px-4 py-2 w-1/3 border-b border-gray-200">Name</th>
+            <th className="px-4 py-2 w-1/6 border-b border-gray-200">Negara</th>
+            <th className="px-4 py-2 w-2/12 border-b border-gray-200">Email</th>
           </tr>
         </thead>
-        <tbody class="text-gray-600">
+        <tbody className="text-gray-600">
           {
             customers.map((customer, index) => (
-              <tr class="hover:bg-gray-200">
-                <td class="px-4 py-2 border-b border-gray-200">{index+1}</td>
-                <td class="px-4 py-2 border-b border-gray-200">{customer.name}</td>
-                <td class="px-4 py-2 border-b border-gray-200">{customer.country.name}</td>
-                <td class="px-4 py-2 border-b border-gray-200">{customer.balance}</td>
+              <tr
+                key={index}
+                className={`hover:bg-gray-200 ${index === selectedIndex ? 'bg-blue-200' : ''}`}
+                onClick={() => tesclick(index)}
+                onDoubleClick={() => handleItemDoubleClick(index)}
+              >
+                <td className="px-4 py-2 border-b border-gray-200">{index + 1}</td>
+                <td className="px-4 py-2 border-b border-gray-200">{customer.name}</td>
+                <td className="px-4 py-2 border-b border-gray-200">{customer.country.name}</td>
+                <td className="px-4 py-2 border-b border-gray-200">{customer.balance}</td>
               </tr>
             ))
           }
-          
+
         </tbody>
       </table>
     </div>
