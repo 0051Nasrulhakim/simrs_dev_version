5aaa34d5d5f8948e71007b95e244b1b61d2aa9b2
0051Nasrulhakim <ti192400051@gmail.com>
2023-12-27 20:02:59 +0700

    fix bg

--- src/app/login-page/layout.js
+++ src/app/login-page/layout.js
@@ -1,11 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './component/login-page.css'
 import '../globals.css'
-// import Image from 'next/image'
+import Image from 'next/image'
 
 export default function RootLayout({ children }) {
   return (
-    <div className='set-bg h-screen'>
+    <div className=' h-screen'>
+      <Image 
+        src="/images/bg-login-type-a.jpg"
+        fill={true}
+        priority={true}
+        
+      />
       <div>
         {children}
       </div>
