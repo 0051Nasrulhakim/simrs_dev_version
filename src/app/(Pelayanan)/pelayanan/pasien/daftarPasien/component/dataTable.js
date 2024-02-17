8f622a5bf695d53623bccf064ec8ae682dce2710
0051Nasrulhakim <ti192400051@gmail.com>
2024-02-17 20:36:16 +0700

    update table

--- src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/component/dataTable.js
+++ src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/component/dataTable.js
@@ -4,7 +4,7 @@ const DataTable = ({ data }) => {
 
   return (
     <>
-      <table className='w-full rounded-md'>
+      <table className='w-[1270px] rounded-md'>
         <thead>
           <tr className='mt-[5%]'>
             <td className='w-[4%] rounded-tl-lg p-3 text-center bg-[#43766C] font-medium text-white'>No</td>
@@ -16,20 +16,24 @@ const DataTable = ({ data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map( (item, index) => (
-            <tr key= { index } >
-              <td className='w-[4%] text-center p-1 border border-b-zinc-700'>{ index + 1 } </td>
-              <td className='w-[16%] px-3 py-2 border border-b-zinc-700'> { item.noBpjs } </td>
-              <td className='w-[13%] px-3 py-2 border border-b-zinc-700'> { item.noRm } </td>
-              <td className='w-[25%] px-3 py-2 border border-b-zinc-700'> { item.namaPasien }</td>
-              <td className='w-[27%] px-3 py-2 border border-b-zinc-700'> { item.alamat } </td>
+          {data.map((item, index) => (
+            <tr key={index} >
+              <td className='w-[4%] text-center p-1 border border-b-zinc-700'>{index + 1} </td>
+              <td className='w-[16%] px-3 py-2 border border-b-zinc-700'> {item.noBpjs} </td>
+              <td className='w-[13%] px-3 py-2 border border-b-zinc-700'> {item.noRm} </td>
+              <td className='w-[25%] px-3 py-2 border border-b-zinc-700'> {item.namaPasien}</td>
+              <td className='w-[27%] px-3 py-2 border border-b-zinc-700'> {item.alamat} </td>
               <td className='w-[15%] px-3 py-2 border border-b-zinc-700'>
-                <div className='flex justify-evenly '>
-                  <div className='bg-yellow-400 rounded-md px-3 py-1 font-medium'>
-                    Ubah
+                <div className='flex justify-center '>
+                  <div className='bg-yellow-400 mr-[3%] rounded-md px-2 py-1 font-medium'>
+                    <button>
+                      Ubah
+                    </button>
                   </div>
-                  <div className='bg-red-600 rounded-md px-3 py-1 text-white font-medium'>
-                    Hapus
+                  <div className='bg-red-600 rounded-md px-2 py-1 text-white font-medium'>
+                    <button>
+                      Hapus
+                    </button>
                   </div>
                 </div>
               </td>
--- src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/page.js
+++ src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/page.js
@@ -1,24 +1,42 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DataTable from './component/dataTable'
 
 export default function page() {
 
+    const [searchval, setSeacrhVal] = useState('');
+    const [typeSearcVal, setTypeSeacrh] = useState('noBpjs');
+
+    // const changeSearch = 
+
     const data = [
-        { 
-            noBpjs: '000000671667266', 
-            noRm: '0009730', 
-            namaPasien: 'Muhammad Ariza juniardi', 
-            alamat: 'Kajen kab. Pekalongan Jawa Tengah' 
+
+        {
+            noBpjs: '000000671667266',
+            noRm: '0009730',
+            namaPasien: 'Muhammad Ariza juniardi',
+            alamat: 'kalirejo Kajen kab. Pekalongan Jawa Tengah'
         },
-        { 
-            noBpjs: '000000671667266', 
-            noRm: '0009730', 
-            namaPasien: 'Muhammad Ariza juniardi', 
-            alamat: 'Kajen kab. Pekalongan Jawa Tengah' 
+        {
+            noBpjs: '000000671615627',
+            noRm: '0009732',
+            namaPasien: 'Maulana Ibrahim',
+            alamat: 'kertijayan Kajen kab. Pekalongan Jawa Tengah'
         },
+        {
+            noBpjs: '000000671818920',
+            noRm: '0009733',
+            namaPasien: 'Ahmad Mirzano',
+            alamat: 'wonoyoso Buaran kab. Pekalongan Jawa Tengah'
+        },
+
     ];
+
+    useEffect(() => {
+        console.log(typeSearcVal)
+    }, [typeSearcVal])
+
 
     return (
         <div>
@@ -33,6 +51,9 @@ export default function page() {
                 <div className='flex'>
                     <div className='bg-[#365486] p-[0.6] rounded-md w-[10%] mr-[0.5%]'>
                         <select id="by" className="h-full w-full rounded-md border-0 bg-transparent py-1 pl-2 pr-3 sm:text-sm text-white"
+                            onChange={
+                                (event) => setTypeSeacrh(event.target.value)
+                            }
                         >
                             <option value="noBpjs" className='text-black'>Nomor BPJS</option>
                             <option value="noRm" className='text-black'>Nomor RM</option>
@@ -41,16 +62,35 @@ export default function page() {
                     </div>
                     <div className='w-[25%]'>
                         <input type="text"
+                            value={searchval}
+                            onChange={
+                                (event) => setSeacrhVal(event.target.value)
+                            }
                             name="Cari" id="Cari" className="block w-full rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 read-only:bg-gray-100 h-[2.37rem]" placeholder="Cari Data Pasien" />
+                    </div>
+                    <div className='py-2 px-2 ml-2 rounded-md text-white bg-blue-500 '>
+                        <button>
+                            Tampilkan
+                        </button>
                     </div>
                 </div>
             </div>
 
-            <div className='tablePasien' id='tablePasien'>
-                <DataTable data={data} />
+            <div className='overflow-x-auto'>
+                <div className='tablePasien' id='tablePasien'>
+                    <DataTable data={data} />
+                </div>
+            </div>
+
+            <div className='mt-6 mb-1 flex justify-center'>
+                <div className='w-auto py-1 px-2 mr-[0.8%] text-center border border-slate-600 rounded-sm bg-slate-600 text-white'>PREV</div>
+                <div className='w-[3.7%] py-1 ml-[0.1%] mr-[0.1%] text-center border border-slate-600 rounded-sm bg-slate-600 text-white'>1</div>
+                <div className='w-[3.7%] py-1 ml-[0.1%] mr-[0.1%] text-center border bg-slate-300 rounded-sm text-black'>2</div>
+                <div className='w-[3.7%] py-1 ml-[0.1%] mr-[0.1%] text-center border border-slate-600 rounded-sm bg-slate-600 text-white'>3</div>
+                <div className='w-[3.7%] py-1 ml-[0.1%] mr-[0.1%] text-center border border-slate-600 rounded-sm bg-slate-600 text-white'>40</div>
+                <div className='w-auto py-1 px-2 ml-[0.8%] text-center border border-slate-600 rounded-sm bg-slate-600 text-white'>NEXT</div>
             </div>
         </div>
-
 
     )
 }
