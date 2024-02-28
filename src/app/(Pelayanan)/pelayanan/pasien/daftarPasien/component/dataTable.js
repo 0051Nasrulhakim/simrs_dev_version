6fb989934b2df75ef91e9bf1cb43692fbdd8a432
0051Nasrulhakim <ti192400051@gmail.com>
2024-02-28 20:07:17 +0700

    Update dataTable.js

--- src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/component/dataTable.js
+++ src/app/(Pelayanan)/pelayanan/pasien/daftarPasien/component/dataTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 
 const DataTable = ({ data }) => {
 
@@ -16,54 +17,65 @@ const DataTable = ({ data }) => {
           </tr>
         </thead>
         <tbody>
-          {data !== undefined ? (
-            data !== null ? (
-              data.length > 0 ? (
-                data.map((item, index) => (
-                  <tr key={index}>
-                    <td className='w-[4%] text-center p-1 border border-b-zinc-700'>{index + 1} </td>
-                    <td className='w-[16%] px-3 py-2 border border-b-zinc-700'> {item.nomor_bpjs} </td>
-                    <td className='w-[13%] px-3 py-2 border border-b-zinc-700'> {item.nomor_rm} </td>
-                    <td className='w-[25%] px-3 py-2 border border-b-zinc-700'> {item.nama_pasien}</td>
-                    <td className='w-[27%] px-3 py-2 border border-b-zinc-700'> {item.alamat} </td>
-                    <td className='w-[15%] px-3 py-2 border border-b-zinc-700'>
-                      <div className='flex justify-center '>
-                        <div className='bg-yellow-400 mr-[3%] rounded-md px-2 py-1 font-medium'>
-                          <button>
-                            Ubah
-                          </button>
-                        </div>
-                        <div className='bg-red-600 rounded-md px-2 py-1 text-white font-medium'>
-                          <button>
-                            Hapus
-                          </button>
-                        </div>
-                      </div>
+          {
+            data !== 401 ? (
+              data !== undefined ? (
+                data !== null ? (
+                  data.length > 0 ? (
+                    data.map((item, index) => (
+                      <tr key={index}>
+                        <td className='w-[4%] text-center p-1 border border-b-zinc-700'>{index + 1} </td>
+                        <td className='w-[16%] px-3 py-2 border border-b-zinc-700'> {item.nomor_bpjs} </td>
+                        <td className='w-[13%] px-3 py-2 border border-b-zinc-700'> {item.nomor_rm} </td>
+                        <td className='w-[25%] px-3 py-2 border border-b-zinc-700'> {item.nama_pasien}</td>
+                        <td className='w-[27%] px-3 py-2 border border-b-zinc-700'> {item.alamat} </td>
+                        <td className='w-[15%] px-3 py-2 border border-b-zinc-700'>
+                          <div className='flex justify-center '>
+                            <div className='bg-yellow-400 mr-[3%] rounded-md px-2 py-1 font-medium'>
+                              <Link href= { `/pelayanan/pasien/updatePasien/${item.nomor_rm}` }>
+                                <button>
+                                  Ubah
+                                </button>
+                              </Link>
+                            </div>
+                            <div className='bg-red-600 rounded-md px-2 py-1 text-white font-medium'>
+                              <button>
+                                Hapus
+                              </button>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="6" className='text-center px-3 py-2 border border-b-zinc-700'>
+                        Loading Data ...
+                      </td>
+                    </tr>
+                  )
+                ) : (
+                  <tr>
+                    <td colSpan="6" className='text-center px-3 py-2 border border-b-zinc-700'>
+                      Data Pasien Tidak Ditemukan
                     </td>
                   </tr>
-                ))
+                )
               ) : (
                 <tr>
                   <td colSpan="6" className='text-center px-3 py-2 border border-b-zinc-700'>
-                    Loading Data ...
+                    Menunggu server untuk merespon
                   </td>
                 </tr>
               )
             ) : (
               <tr>
                 <td colSpan="6" className='text-center px-3 py-2 border border-b-zinc-700'>
-                  Data Pasien Tidak Ditemukan
+                  Anda Belum Melakukan Login !! <br /> Silahkan Login <Link href='/login-page'> KLIK disini </Link>
                 </td>
               </tr>
             )
-
-          ) : (
-            <tr>
-              <td colSpan="6" className='text-center px-3 py-2 border border-b-zinc-700'>
-                Menunggu server untuk merespon
-              </td>
-            </tr>
-          )}
+          }
 
         </tbody>
       </table>
