603856aa54b786b65cf972fa7b3d380b2caedd1d
0051Nasrulhakim <ti192400051@gmail.com>
2024-02-28 20:08:15 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,15 @@ export default function page() {
   return (
     <div>
       <h1>HALAMAN INI MASIH TAHAP PENGEMBANGAN SILAHKAN LANGSUNG MENUJU :</h1>
-      <h3 className='mt-[4%]'>Halaman Dashboard : 
+      <h3 className='mt-[4%]'>Halaman Dashboard :
         <Link href='./pelayanan'> KLIK disini </Link>
       </h3>
 
-      <h3>Halaman Dokter : 
-      <Link href='./dokter'> KLIK disini </Link>
+      <h3>Halaman Dokter :
+        <Link href='./dokter'> KLIK disini </Link>
       </h3>
-      <h3>Halaman Login : 
-      <Link href='./login-page'> KLIK disini </Link>
+      <h3>Halaman Login :
+        <Link href='./login-page'> KLIK disini </Link>
       </h3>
     </div>
   )
